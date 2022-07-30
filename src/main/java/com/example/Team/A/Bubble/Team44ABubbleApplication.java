fedb4fbbe0d191204ff07874b94f4bac807419eb
c21122400 <KhurshidM1@cardiff.ac.uk>
2022-07-31 00:14:55 +0100

    Spring Boot project has been set up

---
+++ src/main/java/com/example/Team/A/Bubble/Team44ABubbleApplication.java
@@ -0,0 +1,13 @@
+package com.example.Team.A.Bubble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team44ABubbleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team44ABubbleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Team/A/Bubble/Team44ABubbleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Team.A.Bubble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team44ABubbleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
