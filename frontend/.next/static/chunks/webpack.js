8f03f4dfc21200d8abfd14fbaa1f0aa38948137a
Ben <70489313+BenWilliams62@users.noreply.github.com>
2022-08-09 10:03:15 +0200

    create-bubble page created

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "34b38e4e3ad5083a"; }
+/******/ 		__webpack_require__.h = function() { return "88c48a520d70ffbb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ frontend/pages/start-streaming/create-bubble.js
@@ -0,0 +1,40 @@
+import Head from "next/head"
+import { Background } from "../../components/background"
+import { Navbar } from "../../components/navbar"
+import styles from '../../styles/StartStreaming.module.css'
+
+const createBubble = () => {
+
+    return (
+        <Background>
+            <Navbar />
+
+            <Head>
+                <title>Bubble - Start Streaming</title>
+                <meta name="description" content="Where live action meets livestream" />
+                <link rel="icon" href="/logo.png" />
+            </Head>
+
+            <div className={styles.container}>
+
+            </div>
+        </Background>
+    )
+
+}
+
+export default createBubble;
+
+export const getServerSideProps = async () => {
+
+    // get user info
+
+    // get location
+
+
+    return {
+        props: {
+
+        }
+    }
+}
