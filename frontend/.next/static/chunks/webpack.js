de92cbff1a64a821a54e002fef371b3cafa41060
Harvey Beynon <harveyibeynon@gmail.com>
2022-08-24 12:14:06 +0100

    Changes the user model to fit the data needed for the sign-up form

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -57,11 +57,6 @@
 /******/ 	__webpack_require__.i = [];
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	!function() {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	}();
-/******/ 	
 /******/ 	/* webpack/runtime/chunk loaded */
 /******/ 	!function() {
 /******/ 		var deferred = [];
@@ -136,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f3285bbd7dfd2911"; }
+/******/ 		__webpack_require__.h = function() { return "494e5c23dab93f61"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
