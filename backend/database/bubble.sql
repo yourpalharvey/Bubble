542993dd9e7667caced1a06036bc5be1c978627d
Ben <70489313+BenWilliams62@users.noreply.github.com>
2022-08-30 20:03:54 +0100

    SQL normalised

--- backend/database/bubble.sql
+++ backend/database/bubble.sql
@@ -1,4 +1,7 @@
+DROP DATABASE IF EXISTS `bubble`;
+
 CREATE DATABASE  IF NOT EXISTS `bubble`;
+
 USE `bubble`;
 
 DROP TABLE IF EXISTS `category`;
@@ -8,42 +11,10 @@ DROP TABLE IF EXISTS `second_tag`;
 DROP TABLE IF EXISTS `third_tag`;
 DROP TABLE IF EXISTS `users`;
 DROP TABLE IF EXISTS `bubbles`;
-
-CREATE TABLE `category`(
-  `category_id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `category_title` varchar(25)
-);
-
-CREATE TABLE `sub_category`(
-  `sub_category_id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `sub_category_title` varchar(25),
-  `category_id` int NOT NULL,
-  CONSTRAINT FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`)
-);
-
-CREATE TABLE `first_tag`(
-  `first_tag_id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `category_id` int NOT NULL,
-  `sub_category_id` int NOT NULL,
-  CONSTRAINT FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`),
-  CONSTRAINT FOREIGN KEY (`sub_category_id`) REFERENCES `sub_category` (`sub_category_id`)
-);
-
-CREATE TABLE `second_tag`(
-  `second_tag_id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `category_id` int NOT NULL,
-  `sub_category_id` int NOT NULL,
-  CONSTRAINT FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`),
-  CONSTRAINT FOREIGN KEY (`sub_category_id`) REFERENCES `sub_category` (`sub_category_id`)
-);
-
-CREATE TABLE `third_tag`(
-  `third_tag_id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `category_id` int NOT NULL,
-  `sub_category_id` int NOT NULL,
-  CONSTRAINT FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`),
-  CONSTRAINT FOREIGN KEY (`sub_category_id`) REFERENCES `sub_category` (`sub_category_id`)
-);
+DROP TABLE IF EXISTS `master_category`;
+DROP TABLE IF EXISTS `tag`;
+DROP TABLE IF EXISTS `bubble_tag`;
+DROP TABLE IF EXISTS `streams`;
 
 CREATE TABLE `users` (
   `id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -51,20 +22,49 @@ CREATE TABLE `users` (
   `password` varchar(25),
   `email` varchar(25),
   `dob` DATE
-  );
-
-CREATE TABLE `bubbles`(
-    `bubble_id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `bubble_title` varchar(25),
-    `category_id` varchar(25),
-    `first_tag_id` int NOT NULL,
-    `second_tag_id` int NOT NULL,
-    `third_tag_id` int NOT NULL,
-    CONSTRAINT FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`),
-    CONSTRAINT FOREIGN KEY (`first_tag_id`) REFERENCES `first_tag` (`first_tag_id`),
-    CONSTRAINT FOREIGN KEY (`second_tag_id`) REFERENCES `second_tag` (`second_tag_id`),
-    CONSTRAINT FOREIGN KEY (`third_tag_id`) REFERENCES `third_tag` (`third_tag_id`)
 );
+
+CREATE TABLE `master_category` (
+  `id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `title` varchar(25)
+);
+
+CREATE TABLE `category` (
+  `id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `title` varchar(25),
+  `master_id` int,
+  FOREIGN KEY (master_id) REFERENCES master_category(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE `bubbles` (
+  `id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `title` varchar(25),
+  `category` int,
+  FOREIGN KEY (category) REFERENCES category(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE `streams` (
+  `id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `signal_id` int NOT NULL,
+  `bubble` int,
+  `user` int,
+  FOREIGN KEY (bubble) REFERENCES bubbles(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (user) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE `tags` (
+  `id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `title` varchar(25)
+);
+
+CREATE TABLE `bubble_tag` (
+  `id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `bubble` int NOT NULL,
+  `tag` int NOT NULL,
+  FOREIGN KEY (bubble) REFERENCES bubbles(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (tag) REFERENCES tags(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
 
 Select * 
 from users
