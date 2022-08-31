709b3892234b7a5f9a9446c0f355ab18737617e1
Ben <70489313+BenWilliams62@users.noreply.github.com>
2022-08-31 17:51:07 +0100

    Category pages get data from database

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3059b19512f56129"; }
+/******/ 		__webpack_require__.h = function() { return "37d7badbed0072d4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
