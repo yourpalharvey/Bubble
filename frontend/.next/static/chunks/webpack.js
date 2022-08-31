6adc8c78a6c4664a9db6aade5051db8ab5f8bc26
Aamir Khan <Khana82@cardiff.ac.uk>
2022-08-31 14:09:59 +0100

    privay policy page created

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0913495f5edfa5f3"; }
+/******/ 		__webpack_require__.h = function() { return "17e52804b3a90534"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ frontend/pages/privacypolicy/index.js
@@ -0,0 +1,19 @@
+import { Background } from "../../components/background";
+import { Navbar } from "../../components/navbar";
+
+export default function PrivacyPolicy () {
+
+    return (
+        <Background>
+            <Navbar />
+
+            <Head>
+                <title>Privacy Policy | Bubble</title>
+                <meta name='description' content='Watch upcoming Events' />
+                <link rel='icon' href='/logo.png' />
+            </Head>
+                
+        </Background>
+    );
+    
+}
